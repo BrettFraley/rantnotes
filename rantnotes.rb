070955aca33e03cdaa168845674378791cac66ee
Brett Fraley <brettfraley@proton.me>
2024-11-23 08:01:27 -0500

    Init rantnotes script for fun...NOTE: speed run tiny features in 15 min intervals..for breaks

---
+++ rantnotes.rb
@@ -0,0 +1,16 @@
+# rantnotes - by Brett Fraley
+# github.io/brettfraley
+# brettfraley.github.io
+
+startup = "R A N T - N O T E S"
+line = "- - - - - - - - - - - - -\n"
+p = "--> "
+s = "Save? (s)"
+puts startup
+puts line
+message = gets.chomp
+
+puts s
+save = gets.chomp
+
+puts s
